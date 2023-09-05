a0f238ed067eb5d0f1c5ad8b4b89644f03d07964
Abdel @ StarkWare <abdelhamid.bakhta@gmail.com>
2023-09-05 10:56:09 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Satoru is a cutting-edge synthetics platform for Starknet, taking inspiration from the modular design of GMX v2.
+      </header>
+    </div>
+  );
+}
+
+export default App;
